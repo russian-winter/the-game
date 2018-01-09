af7b469a1dc2d039ad622c29e13fda97bd2ddb9a
Benjamin Holloway <benjamin.hollo.s@gmail.com>
2018-01-09 20:57:04 -0300

    feat: add space bar to player input

--- game.js
+++ game.js
@@ -28,6 +28,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
   function registerInput(e) {
     const isKeyDown = e.type === 'keydown';
     const action = {
+      32: 'space',
       37: 'left',
       38: 'up',
       39: 'right',
