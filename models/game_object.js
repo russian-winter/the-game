5d23bd659f118b4c9193eb45d1df8ea39da0265d
Benjamin Holloway <benjamin.hollo.s@gmail.com>
2018-01-09 20:57:56 -0300

    feat: pass size as parameter for GameObject

--- models/game_object.js
+++ models/game_object.js
@@ -1,12 +1,12 @@
 class GameObject extends EventEmitter {
-  constructor(position) {
+  constructor(position, size) {
     super();
     this.position = position || new Vector3();
     this.velocity = new Vector3();
     this.model = null;
 
     // Calculate default bounding box
-    const size = new Vector3(0.5, 0.5, 0.5);
+    size = size || new Vector3(0.5, 0.5, 0.5);
     this.boundingBox =
         new BoundingBox(this.position.subtract(size.divide(2)), size);
   }
