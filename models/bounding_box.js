b9264a8ea6517e087a18450bceac7a0704976fea
Benjamin Holloway <benjamin.hollo.s@gmail.com>
2018-01-08 23:43:15 -0300

    feat: add basic BoundingBox intersection

--- models/bounding_box.js
+++ models/bounding_box.js
@@ -3,4 +3,23 @@ class BoundingBox {
         this.position = position || new Vector3();
         this.size = size || new Vector3();
     }
+    /**
+    * Returns true if two BoundingBox intersects, false otherwise.
+    * @boundingBox(object) BoundingBox to check intersection against.
+    * @return
+    *   true if the BoundingBoxes intersected.
+    **/
+    intersects(boundingBox){
+      let intersected_x = false;
+      let intersected_y = false;
+      if((this.position.x <= (boundingBox.position.x + boundingBox.size.x)) &&
+         ((this.position.x + this.size.x) >= boundingBox.position.x)){
+           intersected_x = true;
+      }
+      if((this.position.y <= (boundingBox.position.y + boundingBox.size.y)) &&
+         ((this.position.y + this.size.y) >= boundingBox.position.y)){
+           intersected_y = true;
+      }
+      return intersected_x && intersected_y;
+    }
 }
