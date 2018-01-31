19f06b0f0ad60531971fbbc39b6878f544f08ace
Claudin <claudioberroeta@gmail.com>
2018-01-30 21:58:29 -0300

    disable linter

--- src/renderers/stats.js
+++ src/renderers/stats.js
@@ -3,8 +3,11 @@ export default (context, game) => {
   const yPlayerPosition = game.player.position.y;
   const numOfObjects = game.world.objects.length;
 
+  /* eslint-disable no-param-reassign */
   context.fillStyle = 'rgba(0, 0, 0, 0.5)';
   context.font = '18px Arial';
+  /* eslint-disable no-param-reassign */
+
   context.fillText(`X: ${xPlayerPosition}`, 10, 20);
   context.fillText(`Y: ${yPlayerPosition}`, 10, 50);
   context.fillText(`N: ${numOfObjects}`, 10, 80);
